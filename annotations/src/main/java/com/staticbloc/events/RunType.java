91811f687a1278769a91b12a168784e53f71192b
Eliezer Graber <eygraber@gmail.com>
2015-06-28 16:19:05 -0400

    Fix javadoc

--- annotations/src/main/java/com/staticbloc/events/RunType.java
+++ annotations/src/main/java/com/staticbloc/events/RunType.java
@@ -3,16 +3,16 @@ package com.staticbloc.events;
 public enum RunType {
     /**
      * The event handlers will be called on the same thread that {@code post} was called on.
-     * <br/>
-     * <br/>
+     * <br>
+     * <br>
      * <b>Note:</b> This will block the posting thread. {@link EventHandler}s that perform long running
      * operations should use {@link RunType#BACKGROUND}
      */
     DEFAULT,
     /**
      * The event handlers will be called from the main thread.
-     * <br/>
-     * <br/>
+     * <br>
+     * <br>
      * <b>Note:</b> This will block the posting thread. {@link EventHandler}s that perform long running
      * operations should use {@link RunType#BACKGROUND}
      */
