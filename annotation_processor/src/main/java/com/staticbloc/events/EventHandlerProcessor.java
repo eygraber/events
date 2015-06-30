2c51ca7fefde89d10bef1c72ff4d92c2e2b037f6
Eliezer Graber <eygraber@gmail.com>
2015-06-30 02:25:44 -0400

    Fix the generate class flag in the processor

--- annotation_processor/src/main/java/com/staticbloc/events/EventHandlerProcessor.java
+++ annotation_processor/src/main/java/com/staticbloc/events/EventHandlerProcessor.java
@@ -23,6 +23,8 @@ public class EventHandlerProcessor extends AbstractProcessor {
   private EventComparator eventComparator;
   private Map<String, Set<EventHandlerRegistration>> map;
 
+  boolean generateEventDispatcherFactoryImplementation;
+
   @Override
   public synchronized void init(ProcessingEnvironment processingEnv) {
     super.init(processingEnv);
@@ -30,6 +32,8 @@ public class EventHandlerProcessor extends AbstractProcessor {
     eventHandlerRegistrationParser = new EventHandlerRegistrationParser(processingEnv);
     eventComparator = new EventComparator(processingEnv.getTypeUtils());
     map  = new HashMap<>();
+
+    generateEventDispatcherFactoryImplementation = false;
   }
 
   @Override public SourceVersion getSupportedSourceVersion() {
@@ -38,8 +42,6 @@ public class EventHandlerProcessor extends AbstractProcessor {
 
   @Override
   public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-    boolean generateEventDispatcherFactoryImplementation = false;
-
     for(TypeElement t : annotations) {
       for(Element element : roundEnv.getElementsAnnotatedWith(t)) {
         EventHandlerRegistration eventHandlerRegistration;
